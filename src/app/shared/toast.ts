c30d3c67865f1f27ca6fa637c834fc92989a6b8e
sanikumar sahani <sanikumarsahani@gmail.com>
2021-04-23 15:11:41 +0530

    :hammer: prevent multiple toasts

--- src/app/shared/toast.ts
+++ src/app/shared/toast.ts
@@ -3,15 +3,20 @@ import { ToastController } from '@ionic/angular';
 
 @NgModule({})
 export class ToastExample {
+  private isToastPresent = false;
   constructor(public toastController: ToastController) {}
 
   async displayToast(message: string) {
-    const toast = await this.toastController.create({
-      message,
-      position: 'top',
-      duration: 2000,
-      color: 'primary'
-    });
-    toast.present();
+    if (!this.isToastPresent) {
+      this.isToastPresent = true;
+      const toast = await this.toastController.create({
+        message,
+        position: 'top',
+        duration: 1000,
+        color: 'primary',
+      });
+      toast.present();
+      toast.onDidDismiss().then(() => (this.isToastPresent = false));
+    }
   }
 }
